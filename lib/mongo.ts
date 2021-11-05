4dd364b6bf15349af2f5f296c7eba6e83c7a8376
Dylan Zemlin <dylansmrw@gmail.com>
2021-11-05 13:20:49 -0500

    Fixed some stuff

---
+++ lib/mongo.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+if(process.env.MongoUri == undefined) {
+    throw new Error("MongoUri environment variable is not defined");
+}
+
+let cachedDb: mongoose.Mongoose | undefined = undefined;
+export async function connect(name: string) {
+    if(cachedDb != undefined) {
+        return cachedDb;
+    }
+
+    cachedDb = await mongoose.connect(process.env.MongoUri as string);
+    return cachedDb;
+}
+export default connect
--- pages/api/user/finance.tsx
+++ pages/api/user/finance.tsx
@@ -1,7 +1,11 @@
 import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
+import mongose from '../../../lib/mongo';
 
 export default withApiAuthRequired(function ProtectedRoute(req, res) {
     const session = getSession(req, res);
+    if(session == undefined || session == null) {
+        return res.status(403).json({ code: 403, dataText: "No authorization session found" });
+    }
 
     res.status(200).json({ echoId: session?.user.sub });
 });
