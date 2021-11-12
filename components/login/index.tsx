811bb82d3b0c876eeecb9aa3f999db03f13084ad
Dylan Zemlin <dylansmrw@gmail.com>
2021-11-11 21:09:44 -0600

    Reimplemented auth0

---
+++ components/login/index.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from 'next';
+import Container from '../../modules/container';
+import Styles from './login.module.scss';
+
+const Login: NextPage = () => {
+    return (
+        <Container className={Styles.login}>
+            <h1> Welcome to your financial planner </h1>
+
+            <a href="/api/auth/login">
+                Login
+            </a>
+        </Container>
+    )
+}
+export default Login;
