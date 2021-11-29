3062199c2ab5c78b117edaaceda1c3c808500931
Dylan Zemlin <dylansmrw@gmail.com>
2021-11-29 14:11:34 -0600

    Implemented getStudentAwards function

--- utils/ou-utils.ts
+++ utils/ou-utils.ts
@@ -1,4 +1,41 @@
-export async function getStudentAwards(cookie: string, userId: string) {}
+export async function getStudentAwards(
+	cookie: string,
+	userId: string
+): Promise<StudentAwards | undefined> {
+	// https://api.one.ou.edu/financialaidrespondedawardoffers
+
+	const response = await fetch(
+		"https://api.one.ou.edu/financialaidrespondedawardoffers",
+		{
+			headers: {
+				Cookie: cookie,
+				"X-User-Id": userId,
+			},
+		}
+	);
+
+	if (response.status != 200) {
+		return undefined;
+	}
+
+	return await response.json();
+}
+
+export type StudentAidAward = {
+	name: string;
+	amountOffered: string;
+	amountAccepted: string;
+};
+
+export type StudentAidYear = {
+	aidYearCode: string;
+	aidYearDescription: string;
+	awards: StudentAidAward[];
+};
+
+export type StudentAwards = {
+	aidYears: StudentAidYear[];
+};
 
 export async function getStudentFinances(cookie: string, userId: string) {}
 
