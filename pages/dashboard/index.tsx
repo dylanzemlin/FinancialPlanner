62bf7b996d99a87a5af6a0a0a3ad4d303cbfae46
Dylan Zemlin <dylansmrw@gmail.com>
2021-11-29 17:24:44 -0600

    Fixed a missing space

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -4,12 +4,12 @@ import { NextPage } from "next";
 import * as FinanceUtils from "../../utils/finance-utils";
 
 import {
-	Cell,
-	Legend,
-	Pie,
-	PieChart,
-	ResponsiveContainer,
-	Tooltip,
+    Cell,
+    Legend,
+    Pie,
+    PieChart,
+    ResponsiveContainer,
+    Tooltip,
 } from "recharts";
 import React from "react";
 import Navbar from "../../components/nav";
@@ -20,138 +20,136 @@ import NewUser from "../../modules/dashboard/newuser";
 import ConvertCase from "js-convert-case";
 
 const Dashboard: NextPage = (props) => {
-	const { user, error, isLoading } = useUser();
-	const { response: userData, responseError, fetching } = useApi("/api/user");
-	const {
-		response: financeData,
-		responseError: financeError,
-		fetching: financeFetching,
-	} = useApi("/api/user/finance/");
+    const { user, error, isLoading } = useUser();
+    const { response: userData, responseError, fetching } = useApi("/api/user");
+    const {
+        response: financeData,
+        responseError: financeError,
+        fetching: financeFetching,
+    } = useApi("/api/user/finance/");
 
-	if (error || responseError || financeError) {
-		return <p> Error: {error + " | " + responseError} </p>;
-	}
+    if (error || responseError || financeError) {
+        return <p> Error: {error + " | " + responseError} </p>;
+    }
 
-	if (user == undefined || financeData == undefined) {
-		return <Container title="ENGR 1411 | Dashboard" loading={true} />;
-	}
+    if (user == undefined || financeData == undefined) {
+        return <Container title="ENGR 1411 | Dashboard" loading={true} />;
+    }
 
-	if (userData == undefined || financeFetching) {
-		return <Container title="ENGR 1411 | Dashboard" loading={true} />;
-	}
+    if (userData == undefined || financeFetching) {
+        return <Container title="ENGR 1411 | Dashboard" loading={true} />;
+    }
 
-	if (userData?.code == 404) {
-		return <NewUser />;
-	}
+    if (userData?.code == 404) {
+        return <NewUser />;
+    }
 
-	const date = new Date();
-	const summary = FinanceUtils.calculateMonthlyFinances(date, financeData);
-	let data: Record<
-		string,
-		{
-			name: string;
-			value: number;
-		}[]
-	> = {};
+    const date = new Date();
+    const summary = FinanceUtils.calculateMonthlyFinances(date, financeData);
+    let data: Record<
+        string,
+        {
+            name: string;
+            value: number;
+        }[]
+    > = {};
 
-	let allData: { name: string; value: number }[] = [];
-	for (let key in summary.categoryMap) {
-		if (!(key in data)) {
-			data[key] = [];
-		}
+    let allData: { name: string; value: number }[] = [];
+    for (let key in summary.categoryMap) {
+        if (!(key in data)) {
+            data[key] = [];
+        }
 
-		data[key].push({
-			name: key,
-			value: summary.categoryMap[key],
-		});
-		allData.push({
-			name: ConvertCase.toSentenceCase(key),
-			value: summary.categoryMap[key],
-		});
-	}
+        data[key].push({
+            name: key,
+            value: summary.categoryMap[key],
+        });
+        allData.push({
+            name: ConvertCase.toSentenceCase(key),
+            value: summary.categoryMap[key],
+        });
+    }
 
-	let colorIdx = 0;
-	const colors: string[] = ["#0fafaa", "#581235", "#f9e23e", "#3a2fff"];
+    let colorIdx = 0;
+    const colors: string[] = ["#0fafaa", "#581235", "#f9e23e", "#3a2fff"];
 
-	return (
-		<Container
-			title="ENGR 1411 | Dashboard"
-			className={"flex"}
-			loading={isLoading || fetching}
-		>
-			<Navbar />
+    return (
+        <Container
+            title="ENGR 1411 | Dashboard"
+            className={"flex"}
+            loading={isLoading || fetching}
+        >
+            <Navbar />
 
-			<div style={{ overflowY: "scroll", width: "100%" }}>
-				<h1
-					style={{
-						borderBottom: "2px solid var(--color-bg-secondary)",
-						marginBottom: "0.2rem",
-					}}
-				>
-					Welcome back, {userData.name}!
-				</h1>
+            <div style={{ overflowY: "scroll", width: "100%" }}>
+                <h1
+                    style={{
+                        borderBottom: "2px solid var(--color-bg-secondary)",
+                        marginBottom: "0.2rem",
+                    }}
+                >
+                    Welcome back, {userData.name}!
+                </h1>
 
-				<div style={{ width: "100%", marginBottom: "2rem" }}>
-					<h2>
-						Summary for
-						{new Date().toLocaleString("default", {
-							month: "long",
-						})}
-						({date.getMonth() + 1}/01/{date.getFullYear()} to
-						{date.getMonth() + 1}/{date.getDate()}/
-						{date.getFullYear()}):
-					</h2>
+                <div style={{ width: "100%", marginBottom: "2rem" }}>
+                    <h2>
+                        Summary for {new Date().toLocaleString("default", {
+                            month: "long",
+                        })}
+                        ({date.getMonth() + 1}/01/{date.getFullYear()} to
+                        {date.getMonth() + 1}/{date.getDate()}/
+                        {date.getFullYear()}):
+                    </h2>
 
-					<div className="flex" style={{ gap: "3rem" }}>
-						<div>
-							<h3> Statistics </h3>
-							<p>
-								Gross Income: $
-								{summary.gross.income.toLocaleString()}
-							</p>
-							<p>
-								Gross Expenses: $
-								{summary.gross.expense.toLocaleString()}
-							</p>
-							<p> Profit: ${summary.profit.toLocaleString()} </p>
-						</div>
-					</div>
+                    <div className="flex" style={{ gap: "3rem" }}>
+                        <div>
+                            <h3> Statistics </h3>
+                            <p>
+                                Gross Income: $
+                                {summary.gross.income.toLocaleString()}
+                            </p>
+                            <p>
+                                Gross Expenses: $
+                                {summary.gross.expense.toLocaleString()}
+                            </p>
+                            <p> Profit: ${summary.profit.toLocaleString()} </p>
+                        </div>
+                    </div>
 
-					<h3 style={{ width: "100%", textAlign: "center" }}>
-						Expenses
-					</h3>
-					<ResponsiveContainer width="100%" height={400}>
-						<PieChart>
-							<Pie
-								data={allData}
-								cx="50%"
-								cy="50%"
-								dataKey="value"
-								nameKey="name"
-								stroke="#fff"
-								label={true}
-								labelLine={true}
-							>
-								{[...Object.keys(data)].map((key) => {
-									return (
-										<Cell
-											fill={`${
-												colors[
-													colorIdx++ % colors.length
-												]
-											}`}
-										/>
-									);
-								})}
-							</Pie>
-							<Tooltip />
-							<Legend fill="#fff" />
-						</PieChart>
-					</ResponsiveContainer>
-				</div>
-			</div>
-		</Container>
-	);
+                    <h3 style={{ width: "100%", textAlign: "center" }}>
+                        Expenses
+                    </h3>
+                    <ResponsiveContainer width="100%" height={400}>
+                        <PieChart>
+                            <Pie
+                                data={allData}
+                                cx="50%"
+                                cy="50%"
+                                dataKey="value"
+                                nameKey="name"
+                                stroke="#fff"
+                                label={true}
+                                labelLine={true}
+                            >
+                                {[...Object.keys(data)].map((key) => {
+                                    return (
+                                        <Cell
+                                            fill={`${colors[
+                                                colorIdx++ % colors.length
+                                                ]
+                                                }`}
+                                        />
+                                    );
+                                })}
+                            </Pie>
+                            <Tooltip />
+                            <Legend fill="#fff" />
+                        </PieChart>
+                    </ResponsiveContainer>
+                </div>
+            </div>
+        </Container>
+    );
 };
 
 export default withPageAuthRequired(Dashboard);
