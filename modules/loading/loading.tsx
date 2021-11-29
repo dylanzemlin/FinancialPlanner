283b48f30bec383b027e63d8c32e7c03ddb5c088
Dylan Zemlin <dylansmrw@gmail.com>
2021-11-29 17:00:12 -0600

    Formatting

--- modules/loading/loading.tsx
+++ modules/loading/loading.tsx
@@ -3,17 +3,17 @@ import { NextPage } from "next";
 import Loader from "react-loader-spinner";
 
 const Loading: NextPage = () => {
-	return (
-		<div
-			style={{
-				width: "100%",
-				height: "100vh",
-				backgroundColor: "#121212",
-			}}
-			className={"flex centered"}
-		>
-			<Loader type="ThreeDots" color="#00BFFF" height={100} width={100} />
-		</div>
-	);
+    return (
+        <div
+            style={{
+                width: "100%",
+                height: "100vh",
+                backgroundColor: "#121212",
+            }}
+            className={"flex centered"}
+        >
+            <Loader type="ThreeDots" color="#00BFFF" height={100} width={100} />
+        </div>
+    );
 };
 export default Loading;
--- modules/student/authenticate.tsx
+++ modules/student/authenticate.tsx
@@ -4,72 +4,67 @@ import { toast } from "react-toastify";
 import * as OUtils from "../../utils/ou-utils";
 
 const StudentAuthentication: NextPage = (props) => {
-	const [cookieVal, setCookie] = useState("");
-	const [userIdVal, setUserId] = useState("");
+    const [cookieVal, setCookie] = useState("");
+    const [userIdVal, setUserId] = useState("");
 
-	const authenticateUser = async () => {
-		console.log(cookieVal);
-		console.log(userIdVal);
+    const authenticateUser = async () => {
+        const userData = await fetch("/api/user/student/information", {
+            method: "POST",
+            body: JSON.stringify({
+                cookie: cookieVal,
+                userId: userIdVal,
+            }),
+        });
+        if (userData == undefined) {
+            toast.error(
+                "Failed to fetch OU Student Information, please check your cookie and try again!"
+            );
+            return;
+        }
+    };
 
-		const userData = await fetch("/api/user/student/information", {
-			method: "POST",
-			body: JSON.stringify({
-				cookie: cookieVal,
-				userId: userIdVal,
-			}),
-		});
-		if (userData == undefined) {
-			toast.error(
-				"Failed to fetch OU Student Information, please check your cookie and try again!"
-			);
-			return;
-		}
+    return (
+        <div>
+            <h2> Welcome! </h2>
 
-		console.log(await userData.json());
-	};
+            <p>
+                It seems you have not authenticated with student services yet,
+                please paste your cookie and X-User-Id below!
+            </p>
 
-	return (
-		<div>
-			<h2> Welcome! </h2>
+            <div style={{ gap: "1rem" }} className="flex column">
+                <div className="flex column">
+                    <label htmlFor="cookie"> Cookie </label>
+                    <input
+                        type="text"
+                        id="cookie"
+                        value={cookieVal}
+                        onChange={(e) => setCookie(e.target.value)}
+                        style={{ marginTop: "0.25rem" }}
+                    />
+                </div>
 
-			<p>
-				It seems you have not authenticated with student services yet,
-				please paste your cookie and X-User-Id below!
-			</p>
+                <div className="flex column">
+                    <label htmlFor="userid"> X-User-Id </label>
+                    <input
+                        type="text"
+                        id="userid"
+                        value={userIdVal}
+                        onChange={(e) => setUserId(e.target.value)}
+                        style={{ marginTop: "0.25rem" }}
+                    />
+                </div>
 
-			<div style={{ gap: "1rem" }} className="flex column">
-				<div className="flex column">
-					<label htmlFor="cookie"> Cookie </label>
-					<input
-						type="text"
-						id="cookie"
-						value={cookieVal}
-						onChange={(e) => setCookie(e.target.value)}
-						style={{ marginTop: "0.25rem" }}
-					/>
-				</div>
-
-				<div className="flex column">
-					<label htmlFor="userid"> X-User-Id </label>
-					<input
-						type="text"
-						id="userid"
-						value={userIdVal}
-						onChange={(e) => setUserId(e.target.value)}
-						style={{ marginTop: "0.25rem" }}
-					/>
-				</div>
-
-				<div>
-					<input
-						type="submit"
-						value="Create"
-						onClick={authenticateUser}
-					/>
-				</div>
-			</div>
-		</div>
-	);
+                <div>
+                    <input
+                        type="submit"
+                        value="Create"
+                        onClick={authenticateUser}
+                    />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default StudentAuthentication;
