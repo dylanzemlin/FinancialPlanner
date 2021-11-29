61b35bf201978f62c3409a33e922a0dfbce14325
Dylan Zemlin <dylansmrw@gmail.com>
2021-11-29 13:44:07 -0600

    Implement getStudentInformation utility

--- utils/ou-utils.ts
+++ utils/ou-utils.ts
@@ -1,11 +1,37 @@
-export function getStudentAwards(cookie: string) {
+export async function getStudentAwards(cookie: string, userId: string) {}
 
+export async function getStudentFinances(cookie: string, userId: string) {}
+
+export async function getStudentInformation(
+	cookie: string,
+	userId: string
+): Promise<StudentInformation | undefined> {
+	// https://api.one.ou.edu/PersonalAccountInformation
+
+	const response = await fetch(
+		"https://api.one.ou.edu/PersonalAccountInformation",
+		{
+			headers: {
+				Cookie: cookie,
+				"X-User-Id": userId,
+			},
+		}
+	);
+
+	if (response.status != 200) {
+		return undefined;
+	}
+
+	return await response.json();
 }
 
-export function getStudentFinances(cookie: string) {
-
-}
-
-export function getStudentInformation(cookie: string) {
-
-}
+export type StudentInformation = {
+	dottedEmail: string;
+	emailAlias: string;
+	preferredName: string;
+	firstName: string;
+	soonerId: string;
+	middleName: string;
+	lastName: string;
+	ouNetId: string;
+};
