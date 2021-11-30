9a9b9c097ceb188336b0428c305cadef405cf080
Dylan Zemlin <dylansmrw@gmail.com>
2021-11-30 10:17:39 -0600

    Refactored the look of the home page

--- components/login/index.tsx
+++ components/login/index.tsx
@@ -1,13 +1,21 @@
 import { NextPage } from "next";
 import Container from "../../modules/container";
 import Styles from "./login.module.scss";
+import Image from 'next/image';
 
 const Login: NextPage = () => {
 	return (
 		<Container className={Styles.login}>
-			<h1> Welcome to your financial planner </h1>
+			<div className={Styles.content}>
+                <h1> One Planner, One Student </h1>
+                <p> 
+                    TODO: Fill in some catch phrase thing to attract attention of coming users
+                </p>
 
-			<a href="/api/auth/login">Login</a>
+                <a href="/api/auth/login">Login</a>
+            </div>
+
+            {/* Consider adding a image or something here? */}
 		</Container>
 	);
 };
