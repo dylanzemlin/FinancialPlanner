db826d1685d356b449384dc12c0330a10e72cd13
Dylan Zemlin <dylansmrw@gmail.com>
2021-12-02 03:21:29 -0600

    Changed some API return text

--- pages/api/user/index.tsx
+++ pages/api/user/index.tsx
@@ -6,7 +6,7 @@ import mongoConnect from '../../../lib/mongo';
 export default withApiAuthRequired(async function ProtectedRoute(req, res) {
     const session = getSession(req, res);
     if(session == undefined || session == null) {
-        return res.status(403).json({ code: 403, dataText: "No authorization session found" });
+        return res.status(403).json({ code: 403, dataText: "Not Authorized" });
     }
 
     await mongoConnect();
