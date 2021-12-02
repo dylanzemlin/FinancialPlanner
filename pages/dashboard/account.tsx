70dbaeb6da5ae2db77f3e6f533fd2c84e53ccd0b
Dylan Zemlin <dylansmrw@gmail.com>
2021-12-02 12:25:20 -0600

    Added save account settings

--- pages/dashboard/account.tsx
+++ pages/dashboard/account.tsx
@@ -6,8 +6,30 @@ import Navbar from "../../components/nav";
 import useApi from "../../lib/useApi";
 import Container from "../../modules/container";
 import ResetAccount from "../../popups/dashboard/account/ResetAccount";
+import SaveAccountSettings from "../../popups/dashboard/account/SaveAccount";
 
 const AccountDashboard: NextPage = (props) => {
+
+    const { response, fetching } = useApi("/api/user");
+
+    const [name, setName] = useState(response?.name ?? '');
+    const [age, setAge] = useState(response?.age ?? 18);
+
+    let originalName = "";
+    let originalAge = 18;
+
+    if (response == undefined || fetching == undefined) {
+        return <Container title="ENGR 1411 | Dashboard" loading={true} />;
+    }
+
+    if (name == "") {
+        setName(response?.name);
+        setAge(response?.age);
+
+        originalName = response?.name;
+        originalAge = response?.age;
+    }
+
     return (
         <Container
             title="ENGR 1411 | Student Dashboard"
@@ -30,11 +52,39 @@ const AccountDashboard: NextPage = (props) => {
                     General Settings
                 </h2>
 
-                <div>
+                <div className="flex" style={{ gap: "2rem" }}>
+                    <div className="flex column">
+                        <label htmlFor="firstName"> Full Name </label>
+                        <input
+                            type="text"
+                            id="firstName"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            style={{ marginTop: "0.25rem" }}
+                            placeholder="John"
+                        />
+                    </div>
 
+                    <div className="flex column">
+                        <label htmlFor="age"> Age </label>
+                        <input
+                            type="number"
+                            id="age"
+                            step="any"
+                            min="0"
+                            value={age}
+                            onChange={(e) => setAge(parseInt(e.target.value))}
+                            style={{ marginTop: "0.25rem" }}
+                            placeholder="$0.00"
+                        />
+                    </div>
                 </div>
 
-                <h2 style={{ color: "var(--color-error)" }}>
+                <div style={{ marginTop: "0.3rem" }}>
+                    <SaveAccountSettings editedFields={{ "Full Name": { from: "Dylan Zemlin", to: "John Doe" } }} />
+                </div>
+
+                <h2 style={{ color: "var(--color-error)", marginTop: "3rem" }}>
                     Danger Zone
                 </h2>
 
---
+++ popups/dashboard/account/SaveAccount.tsx
@@ -0,0 +1,47 @@
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { NextPage } from "next";
+import React from "react";
+import Popup from "reactjs-popup";
+
+const SaveAccountSettings: NextPage<{
+    editedFields: Record<string, { from: string, to: string }>
+}> = (props) => {
+
+    return <Popup
+        contentStyle={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+            gap: "1.2rem"
+        }}
+        className="popup"
+        trigger={<button style={{
+            marginTop: "0.4rem",
+            padding: "0.35rem 0.6rem",
+            textAlign: "left",
+            background: "var(--color-table-cell)",
+            border: "1px solid var(--color-primary)"
+        }}> Save Settings </button>}
+        modal
+        nested
+    >
+        <h3 style={{ margin: "0", marginTop: "1.3rem" }}> Save Settings </h3>
+
+        <div>
+            <h3> Changed Fields </h3>
+            {Object.keys(props.editedFields).map(key => {
+                const data = props.editedFields[key];
+                return <p>
+                    <b> {key} </b> { <FontAwesomeIcon icon={faArrowRight}/> } <b> {data.from} </b> to <b> {data.to} </b>.
+                </p>
+            })}
+        </div>
+
+        <div>
+            <input type="submit" value="Confirm" />
+        </div>
+    </Popup>;
+};
+export default SaveAccountSettings;
